fdbe4082f314be5cf7d9b7e2ccae1b91b7cfeb87
Adrian Durran <adrian@intellidroid.eu>
2018-07-02 21:08:39 +0100

    fixed: dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM node:latest
 LABEL maintainer="adrian.durran@digital.cabinet-office.gov.uk"
 ENV NPM_CONFIG_LOGLEVEL warn
 RUN mkdir -p /usr/src/app
+COPY . /usr/src/app
 WORKDIR /usr/src/app
-COPY server/package.json /usr/src/app
 RUN npm install
-COPY /server/. /usr/src/app
-COPY /client/build/. /user/src/app/client
+WORKDIR /usr/src/app/client
+RUN npm install
+RUN npm run build --production
+WORKDIR /usr/src/app
 EXPOSE 5090
 CMD ["node","index.js"]
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+// app.use((req, res, next) => {
+//   const error = new Error('Not found');
+//   error.status = 404;
+//   next(error);
+// });
+
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+// });
+
+app.use(express.static('client/build'));
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5090;
+const server = app.listen(PORT);
+console.log('server listening on ', PORT);
+
+module.exports = app;
